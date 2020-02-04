abe31d54f4105b64f733aaee705f3404f762c4da
Kai Hoffman <hoffm154@purdue.edu>
2020-02-03 19:02:56 -0500

    Implement Angular routing for sign-up pages and add welcome page

--- Rezzi/src/app/app-routing.module.ts
+++ Rezzi/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpHdComponent } from './sign-up-hd/sign-up-hd.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+// Routes
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'sign-up-hd', component: SignUpHdComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Rezzi/src/app/app.module.ts
+++ Rezzi/src/app/app.module.ts
@@ -1,28 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { RouterModule, Routes } from '@angular/router';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignUpHdComponent } from './sign-up-hd/sign-up-hd.component';
-
-const appRoutes: Routes = [
-  { path: 'sign-up-hd', component: SignUpHdComponent }
-
-];
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    SignUpHdComponent
+    SignUpHdComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes, {enableTracing: true, onSameUrlNavigation: 'reload'})
   ],
   providers: [],
   bootstrap: [AppComponent]
