70679fce9260b43fb6dbc2e333779ebe943336cd
ConleyUtz <conleyutz@gmail.com>
2020-02-09 19:53:42 -0500

    Organization of app.module.ts

--- Rezzi/src/app/app.module.ts
+++ Rezzi/src/app/app.module.ts
@@ -24,37 +24,52 @@ import { HttpClientModule } from '@angular/common/http';
 
 // Sign-in / Sign-up Components
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpFormComponent } from './sign-up/sign-up-form/sign-up-form.component';
 import { SignUpHdComponent } from './sign-up-hd/sign-up-hd.component';
+
 import { SignInComponent } from './sign-in/sign-in.component';
-import { SignOutButtonComponent } from './header/sign-out-button/sign-out-button.component';
+import { SignInFormComponent } from './sign-in/sign-in-form/sign-in-form.component';
+import { InfoBlockComponent } from './sign-in/info-block/info-block.component';
+
+
 
 // Index Component (will likely change)
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 // Dashboard Components
-import { EditProfileComponent } from './dashboard/edit-profile/edit-profile.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { EditProfileComponent } from './dashboard/edit-profile/edit-profile.component';
+import { EditProfileFormComponent } from './dashboard/edit-profile/edit-profile-form/edit-profile-form.component';
+
+import { RaAdminComponent } from './dashboard/ra-admin/ra-admin.component';
+import { RaCreateChannelComponent } from './dashboard/ra-admin/ra-create-channel/ra-create-channel.component';
+
+import { CreateRezziComponent } from './dashboard/ha-admin/create-rezzi/create-rezzi.component';
+import { InviteUsersComponent } from './dashboard/ha-admin/invite-users/invite-users.component';
+
 
 // Home Page Components
 import { HomeComponent } from './home/home.component';
 import { InterfaceComponent } from './home/interface/interface.component';
+
 import { SidePanelComponent } from './home/interface/side-panel/side-panel.component';
+import { JoinChannelComponent } from './home/interface/side-panel/join-channel/join-channel.component';
+
 import { ChannelMessagesComponent } from './home/interface/messages/channel-messages/channel-messages.component';
 import { NewMessageComponent } from './home/interface/messages/new-message/new-message.component';
 
+import { ChannelNavBarComponent } from './home/interface/channel-nav-bar/channel-nav-bar.component';
+
+
 // Header Component
 import { HeaderComponent } from './header/header.component';
-import { SignInFormComponent } from './sign-in/sign-in-form/sign-in-form.component';
-import { InfoBlockComponent } from './sign-in/info-block/info-block.component';
-import { SignUpFormComponent } from './sign-up/sign-up-form/sign-up-form.component';
-import { CreateRezziComponent } from './dashboard/ha-admin/create-rezzi/create-rezzi.component';
-import { InviteUsersComponent } from './dashboard/ha-admin/invite-users/invite-users.component';
-import { EditProfileFormComponent } from './dashboard/edit-profile/edit-profile-form/edit-profile-form.component';
-import { ChannelNavBarComponent } from './home/interface/channel-nav-bar/channel-nav-bar.component';
-import { JoinChannelComponent } from './home/interface/side-panel/join-channel/join-channel.component';
-import { RaAdminComponent } from './dashboard/ra-admin/ra-admin.component';
-import { RaCreateChannelComponent } from './dashboard/ra-admin/ra-create-channel/ra-create-channel.component';
+import { SignOutButtonComponent } from './header/sign-out-button/sign-out-button.component';
+
+
+
+
 
 @NgModule({
   declarations: [
