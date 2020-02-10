a3ee94729802285d278cdc109e1494846a1fc841
ConleyUtz <conleyutz@gmail.com>
2020-02-10 17:30:29 -0500

    Added front-end message sending with two-way binding

--- Rezzi/src/app/home/interface/interface.component.ts
+++ Rezzi/src/app/home/interface/interface.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Message } from '../../classes.model';
+
 @Component({
   selector: 'app-interface',
   templateUrl: './interface.component.html',
   styleUrls: ['./interface.component.css']
 })
 export class InterfaceComponent implements OnInit {
-  storedMessages = [];
+  storedMessages: Message[] = [];
 
   onMessageAdded(message) {
+    console.log(message);
     this.storedMessages.push(message);
   }
 
