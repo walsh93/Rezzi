13ae9277e0911a259ef37958d285fccb0a741250
ConleyUtz <conleyutz@gmail.com>
2020-02-10 19:03:45 -0500

    Sign-up form bio checks + message fixing

--- Rezzi/src/app/home/interface/messages/new-message/new-message.component.ts
+++ Rezzi/src/app/home/interface/messages/new-message/new-message.component.ts
@@ -8,8 +8,8 @@ import { Message, User } from '../../../../classes.model';
   styleUrls: ['./new-message.component.css']
 })
 export class NewMessageComponent implements OnInit {
-  enteredMessage = '';
   tempuser = new User('a@a.com', 'abc123', 'Conley', 'Utz', 21, 'CS', 'Con', 'Hi I\'m Conley', true);
+  enteredMessage = '';
   @Output() messageCreated = new EventEmitter<Message>();
 
   onAddMessage() {
