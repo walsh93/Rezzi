5328fcc724aa52a55336e830ccfa502f3aafc9f0
Aaron Lynn <aaronclynn13@gmail.com>
2020-02-24 22:54:05 -0500

    Minor code cleanup in pwordreset, added router to leave reset page. Still needs work to redirect

--- Rezzi/src/app/pwordreset-change/pwordreset-change.component.ts
+++ Rezzi/src/app/pwordreset-change/pwordreset-change.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { User } from 'src/app/classes.model';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pwordreset-change',
@@ -10,12 +11,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PwordresetChangeComponent implements OnInit {
   hide = true;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
 
-  onReset(form: NgForm){
+  onReset(form: NgForm) {
     // Source: CS307 project https://github.com/kbaihoff/CheckedIn
     // Get value of input fields (https://stackoverflow.com/questions/12989741/the-property-value-does-not-exist-on-value-of-type-htmlelement)
     const password = form.value.password;
@@ -26,10 +27,10 @@ export class PwordresetChangeComponent implements OnInit {
 
     // Create XML HTTP Request
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "pword-reset-change", true);
+    xhr.open('POST', 'pword-reset-change', true);
 
-    //Send the proper header information along with the request
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    // Send the proper header information along with the request
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
     /**
      * Call a function when the state changes.
@@ -39,19 +40,19 @@ export class PwordresetChangeComponent implements OnInit {
      * Need credentials = 403, xhr.responseText = <welcome page URL>
      * Firebase error = 500, xhr.responseText = <error message>
      */
-    xhr.onreadystatechange = function () {
-      if (this.readyState === XMLHttpRequest.DONE) {
-        console.log(xhr.status)
-        if (xhr.status == 200 || xhr.status == 403) {
-          window.location.href = xhr.responseText;
-        } else if (xhr.status == 401 || xhr.status == 500) {
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        console.log(xhr.status);
+        if (xhr.status === 200 || xhr.status === 403) {
+          this.router.navigate([xhr.responseText]); // TODO this currently doesn't work, may need to implement sessions and do a refresh
+        } else if (xhr.status === 401 || xhr.status === 500) {
           alert(xhr.responseText);
         } else {
-          console.log(xhr.status)
-          alert("Your POST request received an unhandled response code.");
+          console.log(xhr.status);
+          alert('Your POST request received an unhandled response code.');
         }
       }
-    }
+    };
 
     // Send the request
     xhr.send(body);
