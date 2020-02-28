00bf2068ba0144412085814d1ca41c78669dab9b
Kai Hoffman <hoffm154@purdue.edu>
2020-02-27 21:59:08 -0500

    Get rezzi from database instead of session when inviting users (in case rezzi not in HD document on login)

--- Rezzi/server/routes/invite-users.js
+++ Rezzi/server/routes/invite-users.js
@@ -18,81 +18,82 @@ router.get("/", checkCookie, function(request, response) {
 }).post("/", function(request, response) {
   console.log(request.body);
   const rb = request.body;
-
-  const rezzi = request.__session.rezzi;
   const emailarr = rb.emailList.split(",");
 
-  //for each email in the array, need to save to the db a new user with email, role, floor, rezzi, verified = 0, and pword code
+  db.collection(keys.users).doc(request.__session.email).get().then((doc) => {
+    const rezzi = doc.data().rezzi  // rezzi can't be added to session later on so this will have to do for now
+    //for each email in the array, need to save to the db a new user with email, role, floor, rezzi, verified = 0, and pword code
 
-  db.collection(keys.rezzis + '/' + rezzi + '/floors').doc(rb.floor).update({
-    residents: emailarr
-  });
-  db.collection(keys.rezzis + '/' + rezzi + '/floors/' + rb.floor + '/channels').doc('General').update({
-    members: emailarr
-  });
-  db.collection(keys.rezzis + '/' + rezzi + '/RA').doc('General').update({
-    members: emailarr
-  });
-  db.collection(keys.rezzis + '/' + rezzi + '/hallwide').doc('General').update({
-    members: emailarr
-  });
+    db.collection(keys.rezzis + '/' + rezzi + '/floors').doc(rb.floor).update({
+      residents: emailarr
+    });
+    db.collection(keys.rezzis + '/' + rezzi + '/floors/' + rb.floor + '/channels').doc('General').update({
+      members: emailarr
+    });
+    db.collection(keys.rezzis + '/' + rezzi + '/RA').doc('General').update({
+      members: emailarr
+    });
+    db.collection(keys.rezzis + '/' + rezzi + '/hallwide').doc('General').update({
+      members: emailarr
+    });
 
-  for (var i = 0; i < emailarr.length; i++) {
-    var tempPword = randomstring.generate();
-    var currentEmail = emailarr[i];
-    /*
-      if (db.collection('users').doc(currentEmail).get()){
-        console.log("EXISTS");
-        continue;
-      }
-        TODO: Determine how to check to see if email exists
-      */
-    /*db.collection('users').doc(currentEmail).get().then(doc => {
-        if(doc.exists){
-          response.send(c.EMAIL_ALREADY_REGISTERED)
+    for (var i = 0; i < emailarr.length; i++) {
+      var tempPword = randomstring.generate();
+      var currentEmail = emailarr[i];
+      /*
+        if (db.collection('users').doc(currentEmail).get()){
+          console.log("EXISTS");
+          continue;
+        }
+          TODO: Determine how to check to see if email exists
+        */
+      /*db.collection('users').doc(currentEmail).get().then(doc => {
+          if(doc.exists){
+            response.send(c.EMAIL_ALREADY_REGISTERED)
+          } else {
+            //put things in database*/
+      console.log(rb.floor);
+
+      db.collection(keys.users).doc(currentEmail).set({
+        email: currentEmail,
+        password: tempPword,
+        verified: false,
+        accountType: parseInt(rb.accountType),
+        floor: rb.floor,
+        rezzi: rezzi,
+        channels: rb.channels
+      });
+
+      var smtpTransport = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+          user: "rezzi407@gmail.com",
+          pass: "cs407Project!"
+        }
+      });
+
+      var mailOptions = {
+        to: currentEmail,
+        from: rezzi + " Rezzi <rezzi407@gmail.com>",
+        subject: rezzi + " Rezzi Sign-up",
+        text:
+          "Welcome to Rezzi!\n\nYour hall director has just invited you to the " + rezzi + " Rezzi\n\n" +
+          "To complete the set up procces and activate your account, visit http://localhost:4100/sign-in and use the following credentials:\n\n" +
+          "email: " + currentEmail + "\npassword: " + tempPword + "\n\nYou will be directed to set up your profile and change your password\n\n" +
+          "Have fun connecting with your friends in " + rezzi + " this year!"
+      };
+
+      smtpTransport.sendMail(mailOptions, function(error, res) {
+        if (error) {
+          console.log(`There was an error sending email to ${currentEmail}`)
         } else {
-          //put things in database*/
-    console.log(rb.floor);
+          console.log(`I think we successfully sent an email to ${currentEmail}...???!!!`)
+        }
+      });
+    }
 
-    db.collection(keys.users).doc(currentEmail).set({
-      email: currentEmail,
-      password: tempPword,
-      verified: false,
-      accountType: parseInt(rb.accountType),
-      floor: rb.floor,
-      rezzi: rezzi,
-      channels: rb.channels
-    });
-
-    var smtpTransport = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-        user: "rezzi407@gmail.com",
-        pass: "cs407Project!"
-      }
-    });
-
-    var mailOptions = {
-      to: currentEmail,
-      from: rezzi + " Rezzi <rezzi407@gmail.com>",
-      subject: rezzi + " Rezzi Sign-up",
-      text:
-        "Welcome to Rezzi!\n\nYour hall director has just invited you to the " + rezzi + " Rezzi\n\n" +
-        "To complete the set up procces and activate your account, visit http://localhost:4100/sign-in and use the following credentials:\n\n" +
-        "email: " + currentEmail + "\npassword: " + tempPword + "\n\nYou will be directed to set up your profile and change your password\n\n" +
-        "Have fun connecting with your friends in " + rezzi + " this year!"
-    };
-
-    smtpTransport.sendMail(mailOptions, function(error, res) {
-      if (error) {
-        console.log(`There was an error sending email to ${currentEmail}`)
-      } else {
-        console.log(`I think we successfully sent an email to ${currentEmail}...???!!!`)
-      }
-    });
-  }
-
-  response.status(http.ok).send("An email attempt has been made for each email on the list. No guarentees on success ATM.")
+    response.status(http.ok).send("An email attempt has been made for each email on the list. No guarentees on success ATM.")
+  })
 });
 
 module.exports = router;
