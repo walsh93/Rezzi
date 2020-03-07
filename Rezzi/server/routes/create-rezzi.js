31589a45b435a74308aa11ca749bc079d8eddd7b
Kai Hoffman <hoffm154@purdue.edu>
2020-03-07 12:57:15 -0500

    Change channel naming to match convention in messaging

--- Rezzi/server/routes/create-rezzi.js
+++ Rezzi/server/routes/create-rezzi.js
@@ -17,19 +17,6 @@ router.post('/', checkCookie, function(request, response) {
 
   request.__session.rezzi = req.name
 
-  // Need to redefine the session
-  // savedEmail = request.__session.email
-  // savedVerified = request.__session.verified
-  // savedAccountType = request.__session.accountType
-
-  // request.__session.reset()
-  // request.__session = {
-  //   email: savedEmail,
-  //   verified: savedVerified,
-  //   accountType: savedAccountType,
-  //   rezzi: req.name,
-  // }
-
   db.collection(keys.rezzis).doc(req.name).set({}).then(resolve => {
     // Add floors
     var promises = [];
@@ -46,7 +33,7 @@ router.post('/', checkCookie, function(request, response) {
           promises.push(db.collection(prefix + '/' + floor.name + '/channels').doc(channel.name).set({
             owner: email,  // the HD should be the one currently logged in?
             approvalStatus: true,
-            title: `${req.name}: ${floor.name} ${channel.name}`,
+            title: `${channel.name}`,
             level: 'floor',
             description: '',
             calendar: [],
@@ -64,7 +51,7 @@ router.post('/', checkCookie, function(request, response) {
           promises.push(db.collection(keys.rezzis + '/' + req.name + '/RA').doc(channel.name).set({
             owner: email,  // the HD should be the one currently logged in?
             approvalStatus: true,
-            title: `${req.name}: RA ${channel.name}`,
+            title: `${channel.name}`,
             level: 'RA',
             description: '',
             calendar: [],
@@ -78,7 +65,7 @@ router.post('/', checkCookie, function(request, response) {
           promises.push(db.collection(keys.rezzis + '/' + req.name + '/hallwide').doc(channel.name).set({
             owner: email,  // the HD should be the one currently logged in?
             approvalStatus: true,
-            title: `${req.name}: Hallwide ${channel.name}`,
+            title: `${channel.name}`,
             level: 'hallwide',
             description: '',
             calendar: [],
