761fd88829a9dbeee955e9dd72def41989659b38
Aaron Lynn <aaronclynn13@gmail.com>
2020-03-23 18:05:53 -0400

    Added channel menu, leave buttons to navbar

--- Rezzi/src/app/home/interface/channel-nav-bar/channel-nav-bar.component.ts
+++ Rezzi/src/app/home/interface/channel-nav-bar/channel-nav-bar.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { ChannelNavBarService } from './channel-nav-bar.service';
-
 @Component({
   selector: 'app-channel-nav-bar',
   templateUrl: './channel-nav-bar.component.html',
   styleUrls: ['./channel-nav-bar.component.css']
 })
-export class ChannelNavBarComponent implements OnInit {
 
+export class ChannelNavBarComponent implements OnInit {
   @HostBinding('class.nav-title')
   navTitle = 'Rezzi';
+  channelMenuDisabled = true;
 
-  constructor(private channelNavBarService: ChannelNavBarService) { }
+  constructor(private channelNavBarService: ChannelNavBarService) {}
 
   ngOnInit() {
     this.channelNavBarService.setTitle.subscribe(navTitle => {
       this.navTitle = navTitle;
+      if (this.navTitle !== 'Rezzi') {
+        this.channelMenuDisabled = false;
+      }
     });
   }
 
+  leaveChannel() {
+    if (this.navTitle === 'Rezzi') {
+      console.error('No channel selected');
+      return;
+    }
+    console.log('user wants to leave ' + this.navTitle);
+    // TODO implement leave-channel route
+  }
 }
