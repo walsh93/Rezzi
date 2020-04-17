8c575761bc3b88d2acb38918e6eb41bec4ccf5f2
ConleyUtz <conleyutz@gmail.com>
2020-04-17 18:31:21 -0500

    Added alert saying you can't create a poll without being in a channel

--- Rezzi/src/app/home/interface/channel-nav-bar/channel-nav-bar.component.ts
+++ Rezzi/src/app/home/interface/channel-nav-bar/channel-nav-bar.component.ts
@@ -163,6 +163,7 @@ export class ChannelNavBarComponent implements OnInit, OnDestroy {
   }
 
   openPollDialog(): void {
+    alert("You must select a channel to create a poll!");
     if (this.navTitle === 'Rezzi') {
       console.error('No channel selected');
       return;
