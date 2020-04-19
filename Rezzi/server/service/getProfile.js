2da71ae62af6979e9967c84ff0ae9fa270ebffa1
Aaron Lynn <aaronclynn13@gmail.com>
2020-04-18 20:13:02 -0400

    Copied getUser into getProfile [WIP]

---
+++ Rezzi/server/service/getProfile.js
@@ -0,0 +1,39 @@
+/* TODO this needs to pull any user, not current user */
+
+const express = require('express')
+const router = express.Router()
+const admin = require('firebase-admin')
+const db = admin.firestore()
+const http = require('../constants').http_status;
+
+const checkCookie = require('../permissions').userNeedsToBeLoggedInAndVerified
+
+router.get('/', checkCookie, function (request, response) {
+  db.collection('users').doc(request.__session.email).get().then((doc) => {
+    const data = doc.data()
+    console.log(data)
+
+    const user = {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      password: data.password,
+      major: data.major,
+      nickName: data.nickName,
+      bio: data.bio,
+      age: data.age,
+      deletionRequest: data.deletionRequest,
+      email: data.email,
+      rezzi: data.rezzi,
+      floor: data.floor,  // For request-channel
+      image_url: data.image_url,
+    }
+    response.status(http.ok).json({ user: user })  // will be accessed as data_from_backend in prev code blocks
+  }).catch((error) => {
+    console.log('Error getting documents', error)
+    response.status(http.conflict).json(null)
+  })
+});
+
+
+
+module.exports = router;
